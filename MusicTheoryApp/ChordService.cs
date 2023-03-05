dd0a59ab4f27f1e4a07fc49c7de7b56671a3f087
jamesAforster <jamesalexforster@gmail.com>
2023-03-05 18:07:55 +0000

    Refactor and fix bug

--- MusicTheoryApp/ChordService.cs
+++ MusicTheoryApp/ChordService.cs
@@ -1,25 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MusicTheoryApp
+﻿namespace MusicTheoryApp
 {
     public class ChordService
     {
-        public List<int> MajorIntervals = new List<int>() { 4, 7 };
+        int MajorThirdInterval = 4;
+        int PerfectFifthInterval = 7;
+
         public List<Note> GetMajorChord(Note note)
         {
-            var rootIndex = note.Index; // D 5
-            var thirdIndex = note.Index + MajorIntervals[0] > 11 ? note.Index + MajorIntervals[0] - 12 : note.Index + MajorIntervals[0];
-            var fifthIndex = note.Index + MajorIntervals[1] > 11 ? note.Index + MajorIntervals[1] - 12 : note.Index + MajorIntervals[1];
+            var rootIndex = note.Index;
+            var thirdIndex = GetIndex(rootIndex, MajorThirdInterval);
+            var fifthIndex = GetIndex(rootIndex, PerfectFifthInterval);
 
-            Note root = new Note(rootIndex);
-            Note third = new Note(thirdIndex);
-            Note fifth = new Note(fifthIndex);
+            return new List<Note>
+            {
+                note,
+                new Note(thirdIndex),
+                new Note(thirdIndex)
+            };
+        }
 
-            return new List<Note> { root, third, fifth };
+        private int GetIndex(int index, int halfSteps)
+        {
+            return index + halfSteps > 11 ? index + halfSteps - 12 : index + halfSteps;
         }
     }
 }
