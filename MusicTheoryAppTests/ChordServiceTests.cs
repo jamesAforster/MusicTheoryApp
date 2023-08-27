248327ccc0951dd4179f3ca126826d255271beb7
James Forster <jamesalexforster@gmail.com>
2023-08-27 20:00:38 +0100

    Stoptest from succeeding unsuccessfully

--- MusicTheoryAppTests/ChordServiceTests.cs
+++ MusicTheoryAppTests/ChordServiceTests.cs
@@ -12,24 +12,25 @@ namespace MusicTheoryAppTests
             this.output = output;
         }
     
-        [Fact]
-        public void GetMajorChordReturnsCorrectChord()
+        [Theory]
+        [InlineData("C", "E", "G")]
+        public void GetMajorChordReturnsCorrectChord(string note1, string note2, string note3)
         {
             // Arrange
             var service = new ChordService();
             var note = new Note("C");
             var notes = new List<string>()
             {
-                "C",
-                "E",
-                "D"
+                note1,
+                note2,
+                note3
             };
 
             // Act
             var chord = service.GetMajorChord(note);
 
             // Assert
-            for (int i = 0; i > notes.Count; i++)
+            for (int i = 0; i < notes.Count; i++)
             {
                 Assert.Equal(notes[i], chord[i].Name);
             }
